d5d38ca1b96d582fe193a41d3c57228ac2f8399c
Sachin <sachinbhujbal25@gmail.com>
2023-03-11 13:33:52 +0530

    NEWS APPLICATION

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.news.models.NewsApiResponce;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements SelectListner, Vi
     ProgressDialog progressDialog;
     Button b1,b2,b3,b4,b5,b6,b7;
     SearchView news_Search_View;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -119,16 +121,20 @@ public class MainActivity extends AppCompatActivity implements SelectListner, Vi
     @Override
     public void onBackPressed() {
 
+
+
         AlertDialog.Builder alertdialog = new AlertDialog.Builder(MainActivity.this);
         alertdialog.setTitle("Exit App !!");
         alertdialog.setMessage("Do You Want To Exit App?");
         alertdialog.setCancelable(false);
+        alertdialog.setIcon(R.drawable.main_img);
 
         alertdialog.setPositiveButton(
                 "Yes",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         finish();
+
                     }
                 });
 
--- app/src/main/java/com/example/news/SplashActivity.java
+++ app/src/main/java/com/example/news/SplashActivity.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        Thread thread=new Thread(){
+       Thread  thread=new Thread(){
             public void run(){
                 try {
-                    sleep(4000);
+                    sleep(3000);
+                    finish();
                 }
                 catch (Exception e){
                     // mother of each exceptions
